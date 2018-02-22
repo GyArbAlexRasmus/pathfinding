e5ba4ce88a9c5b798086b0a46218ae0caa8a6db9
holyRa <rasmusbeck2k@outlook.com>
2018-02-22 18:38:53 +0100

    Moved tests namespace into pathfinder namespace

--- Pathfinder/src/tests.cpp
+++ Pathfinder/src/tests.cpp
@@ -4,31 +4,33 @@
 #include <string>
 #include "tests.cpp"
 
-namespace tests {
-    
-    /// Tests a given algorithm
-    /// \param algorithm The algorithm to test
-    /// \return A TestResults struct containing the results of the test, or null if data was not initialized.
-    TestResults RunTest(pathfinder::algorithm algorithm) {
-        TestResults results;
-        
-        std::clock_t start = std::clock();
-        algorithm.findWay();
-        
-        return results;
-    }
-    
-    /// \param results A TestResults struct containing the results of the test
-    void PrintResults(TestResults results) {
-        printf("Time to find path: %ull\n", results.time_elapsed);
-        printf("Edges: %uh\n", results.edges);
-        printf("Total distance of path: %f.0\n", results.distance);
-    }
-    
-    /// Initializes data from a .osm.pbf file
-    /// \param path The path to the file containing the data
-    /// \return A buffer object containing data from the file
-    osmium::memory::Buffer Initialize(std::string path) {
-        
+namespace pathfinder {
+    namespace tests {
+
+        /// Tests a given algorithm
+        /// \param algorithm The algorithm to test
+        /// \return A TestResults struct containing the results of the test, or null if data was not initialized.
+        TestResults RunTest(pathfinder::algorithm algorithm) {
+            TestResults results;
+
+            std::clock_t start = std::clock();
+            algorithm.findWay();
+
+            return results;
+        }
+
+        /// \param results A TestResults struct containing the results of the test
+        void PrintResults(TestResults results) {
+            printf("Time to find path: %ull\n", results.time_elapsed);
+            printf("Edges: %uh\n", results.edges);
+            printf("Total distance of path: %f.0\n", results.distance);
+        }
+
+        /// Initializes data from a .osm.pbf file
+        /// \param path The path to the file containing the data
+        /// \return A buffer object containing data from the file
+        osmium::memory::Buffer Initialize(std::string path) {
+
+        }
     }
 }
--- Pathfinder/src/tests.hpp
+++ Pathfinder/src/tests.hpp
@@ -5,18 +5,20 @@
 #include <chrono>
 #include <osmium/memory/buffer.hpp>
 
-namespace tests {
-    
-    ///\struct Contains the results of a test
-    struct TestResults {
-        uint64_t time_elapsed;
-        uint16_t edges;
-        double distance;
-    };
-    
-    TestResults RunTests();
-    void PrintResults(TestResults results);
-    osmium::memory::Buffer Initialize();
-    
+namespace pathfinder {
+    namespace tests {
+        
+        
+        ///\struct Contains the results of a test
+        struct TestResults {
+            uint64_t time_elapsed;
+            uint16_t edges;
+            double distance;
+        };
+        
+        TestResults RunTests();
+        void PrintResults(TestResults results);
+        void Initialize();
+    }
 }
 #endif /* TESTS_H */
