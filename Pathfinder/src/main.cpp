9001cbe7266f2044c59ecbd7a874658f5d7bb95f
holyRa <rasmusbeck2k@outlook.com>
2018-02-22 15:46:12 +0100

    Moved code out of huge try/catch

--- Pathfinder/src/main.cpp
+++ Pathfinder/src/main.cpp
@@ -5,9 +5,10 @@ namespace po = boost::program_options;
 #include <iterator>
 
 int main(int argc, char* argv[]) {
+    std::string filename;
+    po::variables_map vm;
+    
     try {
-        std::string filename;
-        
         po::options_description desc("Allowed options");
         desc.add_options()
             ("help,h", "produce help message")
@@ -19,32 +20,34 @@ int main(int argc, char* argv[]) {
             ("file,f", po::value<std::string>(&filename), "file from which to extract OSM data")
         ;
 
-        po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
-
-        if (vm.count("help")) {
-            std::cout << desc << "\n";
-            return 0;
-        }
         
-        if (vm.count("version")) {
-            std::cout << "Pathfinder version " << PATHFINDER_VERSION << ".\n";
-            return 0;
-        }
-        
-        if (vm.count("file")) {
-            std::cout << "Set OSM file to " << filename << "\n";
-        } else {
-            std::cout << "You must set a file using --file (-f).\n";
-            return 1;
-        }
     } catch(std::exception& e) {
         std::cerr << "error: " << e.what() << "\n";
         return 1;
     } catch(...) {
         std::cerr << "Exception of unknown type!\n";
     }
+    
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
 
+    if (vm.count("version")) {
+        std::cout << "Pathfinder version " << PATHFINDER_VERSION << ".\n";
+        return 0;
+    }
+
+    if (vm.count("file")) {
+        std::cout << "Set OSM file to " << filename << "\n";
+    } else {
+        std::cout << "You must set a file using --file (-f).\n";
+        return 1;
+    }
+    
+    
+    
     return 0;
 }
