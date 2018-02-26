cf4ed602e6c54d1e37975d951cfcec993ab3eeb8
Rasmus Beck <rasmusbeck2k@outlook.com>
2018-02-26 15:04:06 +0100

    Removed tests namespace from tests.cpp

--- Pathfinder/src/tests.cpp
+++ Pathfinder/src/tests.cpp
@@ -2,40 +2,37 @@
 #include <iostream>
 #include <string>
 
+#include "fileio.hpp"
 #include "pathfinder.hpp"
 #include "tests.hpp"
 
 namespace pathfinder {
-    namespace tests {
-        
-        
-        /// Tests a given algorithm
-        /// \param alg The algorithm to test
-        /// \return A TestResults struct containing the results of the test, or
-        /// null if data was not initialized.
-        TestResults Tests::RunTest(pathfinder::algorithm* alg) {
-            TestResults results;
+    /// Tests a given algorithm
+    /// \param alg The algorithm to test
+    /// \return A TestResults struct containing the results of the test, or
+    /// null if data was not initialized.
+    TestResults Tests::RunTest(pathfinder::algorithm* alg) {
+        TestResults results;
 
-            std::clock_t start = std::clock();
-            algorithm->findWay();
+        std::clock_t start = std::clock();
+        algorithm->findWay();
 
-            return results;
-        }
-        
-        ///Prints the results of a test.
-        /// \param results A TestResults struct containing the results of the test
-        void Tests::PrintResults(TestResults results) {
-            printf("Time to find path: %ull\n", results.time_elapsed);
-            printf("Nodes: %uh\n", results.edges);
-            printf("Total distance of path: %f.0\n", results.distance);
-        }
+        return results;
+    }
 
-        /// Initializes data from a .osm.pbf file
-        /// \param path The path to the file containing the data
-        void Tests::Initialize(std::string path) {
-            graph = new objects::Graph();
-            
-            
-        }
+    ///Prints the results of a test.
+    /// \param results A TestResults struct containing the results of the test
+    void Tests::PrintResults(TestResults results) {
+        printf("Time to find path: %ull\n", results.time_elapsed);
+        printf("Nodes: %uh\n", results.edges);
+        printf("Total distance of path: %f.0\n", results.distance);
+    }
+
+    /// Initializes data from a .osm.pbf file
+    /// \param path The path to the file containing the data
+    void Tests::Initialize(std::string path) {
+        graph = new objects::Graph();
+        fileio::Reader reader();
+
     }
 }
