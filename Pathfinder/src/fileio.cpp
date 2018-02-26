611cce712cb4e313d1e4b2dfd232af600c5fe93a
Rasmus Beck <rasmusbeck2k@outlook.com>
2018-02-26 10:13:24 +0100

    Added function definitions for Reader functions

--- Pathfinder/src/fileio.cpp
+++ Pathfinder/src/fileio.cpp
@@ -5,5 +5,17 @@ namespace pathfinder {
         Reader::Reader(objects::Graph* graph) {
             this->graph = graph;
         }
+        
+        void Reader::Fill() {
+            
+        }
+        
+        objects::Graph* Reader::ReadEdge() {
+            
+        }
+        
+        objects::Node* Reader::ReadNode() {
+            
+        }
     }
 }
