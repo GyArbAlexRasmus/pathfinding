11e5e8baafb6d9e79606cec9ae7800e867acbfff
Rasmus Beck <rasmus.beck@lund.ntig.se>
2018-03-11 16:44:03 +0100

    Document all the Path and new Graph functions

--- Pathfinder/src/pathfinder.cpp
+++ Pathfinder/src/pathfinder.cpp
@@ -39,6 +39,8 @@ namespace pathfinder {
             from_node->adjacent.push_back(edge);
         }
 
+        /// Counts the number of edges in the graph
+        /// \return The number of edges in the graph.
         uint64_t Graph::CountEdges() {
             uint64_t count = 0;
             for (std::pair<id_t, objects::Node*> pair : nodemap) {
@@ -48,10 +50,13 @@ namespace pathfinder {
             return count;
         }
 
+        /// Counts the number of nodes in the graph
+        /// \return The number of nodes in the graph.
         uint64_t Graph::CountNodes() {
             return nodemap.size();
         }
-        
+
+        /// Gets a pointer to a node with a certain ID
         /// \param id The node to find
         /// \return A pointer to the Node with the given ID, or NULL if there
         /// is no such node.
@@ -59,7 +64,8 @@ namespace pathfinder {
             auto node_iter = nodemap.find(id);
             return node_iter != nodemap.end() ? node_iter->second : NULL;
         }
-        
+
+        /// Gets the cost of the edge between two nodes
         /// \param src Source node ID
         /// \param target Target node ID
         /// \return The cost of the cheapest edge between src and target, or
@@ -81,7 +87,8 @@ namespace pathfinder {
             
             return cheapest;
         }
-        
+
+        /// Checks if the graph has a node with a certain ID
         /// \param id ID of node to look for
         /// \return True if graph has a node with given ID; otherwise false
         bool Graph::HasNode(id_t id) {
@@ -134,12 +141,15 @@ namespace pathfinder {
                     src_n->adjacent.erase(iter);
             }
         }
-        
+
+        /// \param graph A pointer to the \c Graph on which to build the path
         Path::Path(Graph* graph) :
             nodes() {
             this->graph = graph;
         }
-        
+
+        /// Checks if the path is fully connected
+        /// \return True if the path is fully connected, otherwise false
         bool Path::IsConnected() {
             for(int i = 0; i < Size() - 1; i++) {
                 
@@ -151,6 +161,8 @@ namespace pathfinder {
             return true;
         }
 
+        /// Gets the total cost of the path
+        /// \return The total cost of the path
         objects::cost_t Path::GetCost() {
             cost_t cost = 0;
             
@@ -167,10 +179,16 @@ namespace pathfinder {
             return cost;
         }
 
+        /// Removes the last node from the path
         void Path::Pop() {
             nodes.pop_back();
         }
-        
+
+        /// Adds a node to the path
+        /// \param id The ID of a node to add
+        /// \throws std::logic_error if there is no node with the given ID or
+        /// if there's no valid edge between the current top node and the given
+        /// node
         void Path::Push(id_t id) {
             if(!graph->HasNode(id))
                 throw std::logic_error("Path::Push: No node with given ID");
@@ -180,11 +198,15 @@ namespace pathfinder {
             
             nodes.push_back(id);
         }
-        
+
+        /// Gets the top node ID
+        /// \return The ID of the top node
         id_t Path::Top() {
             return nodes[nodes.size() - 1];
         }
-        
+
+        /// Gets the total number of nodes in the path
+        /// \return The number of nodes in the path
         size_t Path::Size() {
             return nodes.size();
         }
