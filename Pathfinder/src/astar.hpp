1f8a10918992ff25b768e2ab854806b3a08b8104
Rasmus Beck <rasmus.beck@lund.ntig.se>
2018-03-14 12:48:26 +0100

    Change A* to a priority queue

--- Pathfinder/src/astar.hpp
+++ Pathfinder/src/astar.hpp
@@ -1,15 +1,53 @@
 #ifndef ASTAR_HPP
 #define ASTAR_HPP
 
+#include <queue>
+
 namespace pathfinder {
     namespace algorithms {
         class AStar : public Algorithm {
         private:
+            struct CustomPrioQueue {
+                typedef std::pair<objects::id_t, objects::cost_t> pair;
+
+                struct comp {
+                    bool operator()(const pair a, const pair b) {
+                        return a.second > b.second;
+                    }
+                };
+
+                std::priority_queue<pair, std::vector<pair>, comp> prioqueue;
+
+                inline bool empty() {
+                    return prioqueue.empty();
+                }
+
+                inline objects::id_t get() {
+                    objects::id_t id = top();
+                    pop();
+
+                    return id;
+                }
+
+                inline objects::id_t top() {
+                    return prioqueue.top().first;
+                }
+
+                inline void pop() {
+                    prioqueue.pop();
+                }
+
+                inline void push(objects::id_t id, objects::cost_t cost) {
+                    pair p(id, cost);
+                    prioqueue.push(p);
+                }
+            };
+
             // The nodes which have already been checked
             std::vector<objects::id_t> closedSet;
 
             // The set of discovered nodes that have not been checked.
-            std::vector<objects::id_t> openSet;
+            CustomPrioQueue openSet;
 
             // Contains the node from which a given node is best reached
             std::map<objects::id_t, objects::id_t> bestReachedFrom;
@@ -17,12 +55,7 @@ namespace pathfinder {
             // The lowest cost to get to a given node from src
             std::map<objects::id_t, objects::cost_t> costToMap;
 
-            // The cost to get from the given node to the end
-            std::map<objects::id_t, objects::cost_t> costFromMap;
-
-            objects::cost_t GetCostFrom(objects::id_t target);
             objects::cost_t GetCostTo(objects::id_t target);
-            objects::id_t GetCheapestNodeInOpenSet();
             bool IsInClosedSet(objects::id_t);
             bool IsInOpenSet(objects::id_t);
             objects::cost_t Heuristic(objects::id_t src, objects::id_t target);
